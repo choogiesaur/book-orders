e3ea0facf28d1a0d4a1585c4d3c18d77bfd97c88
choogiesaur <choogiesaur@gmail.com>
2014-11-18 18:58:19 -0500

    Create customer-database.h
    omar's magical header file

---
+++ customer-database.h
@@ -0,0 +1,57 @@
+#ifndef CUSTOMER_DATABASE_H
+#define CUSTOMER_DATABASE_H
+/*
+ * customer-database.h
+ */
+
+#include <stdlib.h>
+
+struct sorder {
+	char *bname;
+	double price;
+	double updatedcredit;
+	struct sorder *next;
+};
+typedef struct sorder SOrder;
+
+struct rorder {
+	char *bname;
+	double price;
+	struct rorder *next;
+};
+typedef struct rorder ROrder;
+
+struct customer {
+	char *name;
+	int id;
+	double credit;
+	char *address;
+	char *state;
+	char *zip;
+	struct sorder *slist;
+	struct rorder *rlist;
+};
+typedef struct customer Customer;
+
+struct customerdatabase {
+	int numCust;
+	int dbSize;
+	struct customer *dbarray;
+};
+typedef struct customerdatabase* CDB;
+
+int SLDestroy(SOrder *front);
+
+int RLDestroy(ROrder *front);
+
+CDB CDCreate();
+
+int CDDestroy(CDB cdb);
+
+int CDInsert(CDB cdb, struct customer *cust);
+
+int CDUpdate(CDB cdb, char *bookname, double price);
+
+void PrintDB(CDB cdb);
+
+#endif
