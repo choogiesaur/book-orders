a67f67c3223e2f1aef88529fc54f9de7c93bf900
obakir <wldkng14@yahoo.com>
2014-11-21 17:35:14 -0500

    Create queue.h

---
+++ queue.h
@@ -0,0 +1,32 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+/*
+ * queue.h
+ */
+
+#include <stdlib.h>
+
+struct qnode {
+	char *bname;
+	double price;
+	long id;
+	char *category;
+	struct qnode *next;
+};
+typedef struct qnode QNode;
+
+struct queue {
+	int numElem;
+	int max;
+	struct qnode *front;
+	struct qnode *back;
+};
+typedef struct queue* Queue;
+
+Queue QCreate();
+
+int QDestroy(Queue *front);
+
+QNode push(Queue *q);
+
+QNode pop(Queue *q);
