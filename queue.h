957094fbba7c5432465852f42446fe0296d637a5
obakir <wldkng14@yahoo.com>
2014-11-21 18:40:38 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -25,10 +25,10 @@ typedef struct queue* Queue;
 
 Queue QCreate();
 
-int QDestroy(Queue front);
+int QDestroy(Queue q);
 
-QNode push(Queue q);
+int push(Queue q, QNode *n);
 
-QNode pop(Queue q);
+QNode *pop(Queue q);
 
 #endif
