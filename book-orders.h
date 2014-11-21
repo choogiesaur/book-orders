f373b8d6fdf0215cbac346c9eb79846a7e12cf08
Firas S. <choogiesaur@gmail.com>
2014-11-21 18:59:20 -0500

    Update book-orders.h

--- book-orders.h
+++ book-orders.h
@@ -1,3 +1,9 @@
+#ifndef BOOK_ORDERS_H
+#define BOOK_ORDERS_H
+/*
+ * book-orders.h
+ */
+
 //aint nuttin here yet...
 
 CDB read_customers(CDB cdb, char *filename);
@@ -5,3 +11,5 @@ CDB read_customers(CDB cdb, char *filename);
 CSA read_categories(CSA csa, char *filename);
 
 void printCustomer(Customer *dude);
+
+#endif
