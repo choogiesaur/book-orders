5b7ebcb6a57abe5f3f6654da5898aab50c0589ce
obakir <wldkng14@yahoo.com>
2014-11-24 22:09:17 -0500

    Update book-orders.h

--- book-orders.h
+++ book-orders.h
@@ -4,13 +4,11 @@
  * book-orders.h
  */
 
-//aint nuttin here yet...
-
 CDB read_customers(CDB cdb, char *filename);
 
 CSA read_categories(CSA csa, char *filename);
 
-int read_orders(char *filename);
+int producer(CSA csa, char *filename);
 
 void printCustomer(Customer *dude);
 
