fe98355506499fd5a0703bc7a5ec792fb62206ed
obakir <wldkng14@yahoo.com>
2014-11-25 18:29:36 -0500

    Update customer-database.c

--- customer-database.c
+++ customer-database.c
@@ -1,273 +1,231 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
-#include <unistd.h>
 #include "customer-database.h"
-#include "consumer-database.h"
-#include "book-orders.h"
-#include "queue.h"
-//MAIN PROG ARGS: ./book-orders <cust database input file> <book order input file> <categories files>
+#include <string.h>
+#include <stdio.h>
 
-int main(int argc, char **argv){
-	char *cust_file = argv[1];
-	char *orders_file = argv[2];
-	char *categories_file = argv[3];
-	
-	printf("customer file: %s\n", cust_file);
-	printf("order file: %s\n", orders_file);
-	printf("categories file: %s\n", categories_file);
-	printf("\n");
-	
-	CDB tcdb = CDCreate();
-	tcdb = read_customers(tcdb, cust_file); //first fxn
-	cdb = tcdb;
-	
-	CSA tcsa = CSACreate();
-	tcsa = read_categories(tcsa, categories_file); //2nd fxn
-	csa = tcsa;
-	
-	pthread_t tid[csa->numCons + 1];
-	pthread_create(&tid[0], NULL, producer, orders_file);
-	
-	int i;
-	for (i = 0; i < csa->numCons; i++) {
-		pthread_create(&tid[i + 1], NULL, consumer, &(csa->consumerdata[i]));
+int binarySearch(CDB cdb, long key, int low, int high) {
+    if (high < low) {
+        return -1;
 	}
-	
-	for (i = 0; i < csa->numCons + 1; i++) {
-		pthread_join(tid[i], NULL);
+	int mid;
+    mid = low + ((high - low) / 2);
+    if (cdb->dbarray[mid].id == key) {
+		return mid;
+	} else if (cdb->dbarray[mid].id < key) {
+		return binarySearch(cdb, key, (mid + 1), high);
+	} else if (cdb->dbarray[mid].id > key) {
+		return binarySearch(cdb, key, low, (mid - 1));
 	}
-	
-	PrintDB(cdb);
-	return 0;
+	return -1;
 }
 
-CDB read_customers(CDB cdb, char *filename){ //cdb is the customer database ptr you got in this fxn
-	
-	FILE *customer_file;
-	customer_file = fopen(filename, "r");
-	
-	if (customer_file == NULL){ //obv checking if null like DUH
-    		printf("ERR: could not read file %s", filename);
-    		return NULL;
+int customercomp(const void *a, const void *b) {
+	Customer *tempa = (Customer *)a;
+	Customer *tempb = (Customer *)b;
+	if (tempa->id - tempb->id > 0) {
+		return 1;
 	}
-	
-	char line[300]; //remember MAY NEED TO MODIFY
-	const char delims[2] = "|\n";
-	//cdb = CDCreate();
-	while(fgets(line, 300, customer_file) != NULL){ //stored in 'line'
-		printf("line: %s", line);
-		
-		Customer *cust =(Customer *) malloc(sizeof(Customer));
-		char *token;
-		
-		char *name;
-		long id;// char id;
-		double balance;// char balance;
-		char *address;
-		char *state;
-		char *zip;
-		
-		token = strtok(line, delims); //name
-		name =(char *) malloc(strlen(token) + 1);
-		strcpy(name, token);
-		name[strlen(token)] = '\0';
-		printf("	name: %s\n", name);
-		
-		token = strtok(NULL, delims); //id
-		id = strtol(token, NULL, 10);
-		printf("	id: %ld\n", id);
-		
-		token = strtok(NULL, delims); //balance
-		balance = strtod(token, NULL);
-		printf("	balance: %f\n", balance);
-		
-		token = strtok(NULL, delims);
-		address =(char *) malloc(strlen(token) + 1);
-		strcpy(address, token);
-		address[strlen(token)] = '\0';
-		printf("	addr: %s\n", address);
-		
-		token = strtok(NULL, delims);
-		state =(char *) malloc(strlen(token) + 1);
-		strcpy(state, token);
-		state[strlen(token)] = '\0';
-		printf("	state: %s\n", state);
-		
-		token = strtok(NULL, delims);
-		zip =(char *) malloc(strlen(token) + 1);
-		strcpy(zip, token);
-		zip[strlen(token)] = '\0';
-		printf("	zip: %s\n", zip);
-		
-		cust->name = name;
-		cust->id = id;
-		cust->balance = balance;
-		cust->address = address;
-		cust->state = state;
-		cust->zip = zip;
-		
-		//printCustomer(cust);
-		CDInsert(cdb, cust);
-				
+	else if (tempa->id - tempb->id < 0) {
+		return -1;
 	}
-	void *vcdb = (void *) cdb->dbarray; //vcdb is the actual customer array, casted to a void (doesnt have the cdb wrapper)
-	qsort(vcdb,cdb->numCust,sizeof(Customer),customercomp);
-	cdb->dbarray = (Customer *) vcdb;
+	else {
+		return 0;
+	}
+} 
+
+int SLDestroy(SOrder *front){
+	if (front == NULL) {
+		printf("Error: SList is NULL.\n");
+		return 0;
+	}
+	SOrder *curr;
+	SOrder *temp;
+	curr = front;
+	while (curr != NULL) {
+		free(curr->bname);
+		temp = curr;
+		curr = curr->next;
+		free(temp);
+	}
+	return 1;
+}
+
+int RLDestroy(ROrder *front){
+	if (front == NULL) {
+		printf("Error: RList is NULL.\n");
+		return 0;
+	}
+	ROrder *curr;
+	ROrder *temp;
+	curr = front;
+	while (curr != NULL) {
+		free(curr->bname);
+		temp = curr;
+		curr = curr->next;
+		free(temp);
+	}
+	return 1;
+}
+
+CDB CDCreate() {
+	// Creating and mallocing the CDB to be returned via the pointer cdb.
+	CDB cdb;
+	cdb = (struct customerdatabase *)malloc(sizeof(struct customerdatabase));
+	// If malloc fails print an error statement and return NULL.
+	if (cdb == NULL) {
+		printf("Error: Out of memory.\n");
+		return NULL;
+	}
+	cdb->dbarray = (Customer *)malloc(10 * sizeof(Customer));
+	cdb->numCust = 0;
+	cdb->dbSize = 10;
 	return cdb;
 }
 
-CSA read_categories(CSA csa, char *filename){ //reads in the categories textfile
-
-	FILE *categories_file;
-	categories_file = fopen(filename, "r");
-	
-	if (categories_file == NULL){ //obv checking if null like DUH
-    		printf("ERR: could not read file %s\n", filename);
-    		return NULL;
+int CDDestroy(CDB cdb) {
+	if (cdb == NULL) {
+		printf("Error: CDB is NULL.\n");
+		return 0;
 	}
-	
-	char line[200]; //remember MAY NEED TO MODIFY
-	const char delims[2] = "\n ";
-	
-	while(fgets(line, 200, categories_file) != NULL){ //stored in 'line'
-	
-		//ConsumerStruct *consumer =(ConsumerStruct *) malloc(sizeof(ConsumerStruct));
-		
-		printf("line: %s", line);
-		char *token;
-		token = strtok(line, delims); //name
-		char *category = (char *) malloc(strlen(token) + 1);
-		strcpy(category, token);
-		category[strlen(token)] = '\0';
-		printf("	category: '%s'\n", category);
-		
-		//consumer->category = category;
-		CSAInsert(csa, category);
+	int i;
+	int flag = 0;
+	for (i = 0; i < cdb->numCust; i++) {
+		free(cdb->dbarray[cdb->numCust].name);
+		free(cdb->dbarray[cdb->numCust].address);
+		free(cdb->dbarray[cdb->numCust].state);
+		free(cdb->dbarray[cdb->numCust].zip);
+		if (!(flag = SLDestroy(cdb->dbarray[cdb->numCust].slist))) {
+			printf("Error: Cannot free SList.\n");
+		}
+		if (!(flag = RLDestroy(cdb->dbarray[cdb->numCust].rlist))) {
+			printf("Error: Cannot free RList.\n");
+		}
 	}
-	void *vcsa = (void *) csa->consumerdata; //vcsa is the actual consumerdata array, casted to a void (doesnt have the csa wrapper)
-	qsort(vcsa,csa->numCons,sizeof(ConsumerStruct),consumercomp);
-	csa->consumerdata = (ConsumerStruct *) vcsa;
-	return csa;
-
+	return 1;
 }
 
-void *producer(char *filename){
-	printf("--------ORDERS--------\n");
-	FILE *orders_file;
-	orders_file = fopen(filename, "r");
-	
-	if (orders_file == NULL){
-    		printf("ERR: could not read orders file %s\n", filename);
-    		return NULL;
+int CDInsert(CDB cdb, Customer *cust) {
+	if (cdb == NULL) {
+		printf("Error: Customer DataBase is NULL.\n");
+		return 0;
 	}
-	
-	char line[300];
-	const char delims[2] = "|\n";
-	
-	pthread_detach(pthread_self());
-	while(fgets(line, 300, orders_file) != NULL){ //each line is an order
-		int index;
-		QNode *order;
-		order = (QNode *)malloc(sizeof(QNode));
-		printf("line: %s", line);
-		char *token;
+	if (cust == NULL) {
+		printf("Error: Customer in insert is NULL.\n");
+		return 0;
+	}
+	if (cdb->numCust == cdb->dbSize) {
+		cdb->dbarray = realloc(cdb->dbarray, 2 * cdb->dbSize);
+		if (cdb->dbarray == NULL) {
+			printf("Error: Realloc of DataBase is NULL.\n");
+			return 0;
+		}
+		cdb->dbSize = 2 * cdb->dbSize;
+	}
+	if (pthread_mutex_init(&(cdb->dbarray[cdb->numCust].queue_mutex), 0) != 0) {
+		printf("Error: Cannot initialize mutex.\n");
+	}
+	cdb->dbarray[cdb->numCust].name = cust->name;
+	cdb->dbarray[cdb->numCust].id = cust->id;
+	cdb->dbarray[cdb->numCust].balance = cust->balance;
+	cdb->dbarray[cdb->numCust].address = cust->address;
+	cdb->dbarray[cdb->numCust].state = cust->state;
+	cdb->dbarray[cdb->numCust].zip = cust->zip;
+	cdb->dbarray[cdb->numCust].slist = NULL;
+	cdb->dbarray[cdb->numCust].rlist = NULL;
+	cdb->numCust++;
+	return 1;
+}
 
-		char *name;	//book title
-		double cost;	//book cost
-		long id;	//customer id
-		char *category;	//book category
-		
-		token = strtok(line, delims); //name
-		name =(char *) malloc(strlen(token) + 1);
-		strcpy(name, token);
-		name[strlen(token)] = '\0';
-		printf("	title: %s\n", name);
+int CDUpdate(CDB cdb, QNode *order) {
+	int index;
+	Customer *cust;
+	index = binarySearch(cdb, order->id, 0, cdb->numCust - 1);
+	if (index == -1) {
+		printf("Error: customer not found in CDUpdate.");
+		return 0;
+	}
+	cust = &(cdb->dbarray[index]);
+	if (order->price > cdb->dbarray[index].balance) {
+		ROrder *rejected;
+		char *bookname;
+		rejected = (ROrder *)malloc(sizeof(ROrder));
+		bookname = (char *)malloc(sizeof(order->bname) + 1);
+		strcpy(bookname, order->bname);
+		rejected->bname = bookname;
+		rejected->price = order->price;
+		rejected->next = NULL;
+		if (cdb->dbarray[index].rlist == NULL) {
+			cdb->dbarray[index].rlist = rejected;
+			cdb->dbarray[index].rlistback = rejected;
+		} 
+		else {
+			cdb->dbarray[index].rlistback->next = rejected;
+			cdb->dbarray[index].rlistback = rejected;
+		}
+	}
+	else {
+		SOrder *accepted;
+		char *bookname;
+		accepted = (SOrder *)malloc(sizeof(SOrder));
+		bookname = (char *)malloc(sizeof(order->bname) + 1);
+		strcpy(bookname, order->bname);
+		accepted->bname = bookname;
+		accepted->price = order->price;
+		accepted->updatedbalance = cdb->dbarray[index].balance - order->price;
+		accepted->next = NULL;
+		if (cdb->dbarray[index].slist == NULL) {
+			cdb->dbarray[index].slist = accepted;
+			cdb->dbarray[index].slistback = accepted;
+		} 
+		else {
+			cdb->dbarray[index].slistback->next = accepted;
+			cdb->dbarray[index].slistback = accepted;
+		}
+		cdb->dbarray[index].balance = accepted->updatedbalance;
+		pthread_mutex_lock(&cdb->revenuemutex);
+		cdb->revenue += order->price;
+		pthread_mutex_unlock(&cdb->revenuemutex);
+	}
+	return 1;
+}
 
-		token = strtok(NULL, delims); //balance
-		cost = strtod(token, NULL);
-		printf("	cost: %f\n", cost);
-
-		token = strtok(NULL, delims); //id
-		id = strtol(token, NULL, 10);
-		printf("	customer id: %ld\n", id);
-
-		token = strtok(line, delims); //name
-		category =(char *) malloc(strlen(token) + 1);
-		strcpy(category, token);
-		category[strlen(token)] = '\0';
-		printf("	category: %s\n", category);
-		
-		order->bname = name;
-		order->price = cost;
-		order->id = id;
-		order->category = category;
-		order->next = NULL;
-		index = binarySearch2(csa, order->category, 0, csa->numCons);
-		if (index >= 0) {
-			pthread_mutex_lock(&csa->consumerdata[index].mutex);
-			while (csa->consumerdata[index].q->numElem == csa->consumerdata[index].q->max)
-			{
-				pthread_cond_signal(&csa->consumerdata[index].dataAvailable); // shout at consumer
-				printf("Producer waits for consumer thread '%s' because of full queue.\n", csa->consumerdata[index].category);
-				pthread_cond_wait(&csa->consumerdata[index].spaceAvailable, &csa->consumerdata[index].mutex);
+void PrintDB(CDB cdb) {
+	if (cdb == NULL) {
+		printf("Error: CDB is NULL.\n");
+		return;
+	}
+	printf("=== TOTAL REVENUE GENERATED: %f ===\n\n", cdb->revenue);
+	int i;
+	for (i = 0; i < cdb->numCust; i++) {
+		printf("=== BEGIN CUSTOMER INFO ===\n");
+		printf("### BALANCE ###\n");
+		printf("Customer name: %s\n", cdb->dbarray[i].name);
+		printf("Customer ID number: %ld\n", cdb->dbarray[i].id);
+		printf("Remaining credit balance after all purchases (a dollar amount): %f\n", cdb->dbarray[i].balance);
+		printf("### SUCCESSFUL ORDERS ###\n");
+		if (cdb->dbarray[i].slist != NULL) {
+			SOrder *curr;
+			curr = cdb->dbarray[i].slist;
+			while (curr != NULL) {
+				printf("%s|%f|%f\n", curr->bname, curr->price, curr->updatedbalance);
+				curr = curr->next;
 			}
-			printf("Producer resuming creation of orders for consumer thread '%s'.\n", csa->consumerdata[index].category);
-			if(push(csa->consumerdata[index].q, order) == 0) {
-				printf("Error: Push failed.\n");
-				return NULL;
+		}
+		else {
+			printf("=== EMPTY LIST ===\n");
+		}
+		printf("### REJECTED ORDERS ###\n");
+		if (cdb->dbarray[i].rlist != NULL) {
+			ROrder *curr;
+			curr = cdb->dbarray[i].rlist;
+			while (curr != NULL) {
+				printf("%s|%f\n", curr->bname, curr->price);
+				curr = curr->next;
 			}
-			pthread_cond_signal(&csa->consumerdata[index].dataAvailable);
-			pthread_mutex_unlock(&csa->consumerdata[index].mutex);
 		}
-	}
-	pthread_cond_signal(&csa->done);
-	return NULL;
-}
-
-void *consumer(ConsumerStruct *consumerstruct) {
-	if (cdb == NULL || consumerstruct == NULL) {
-		printf("ERROR: CDB or consumer struct NULL.\n");
-		return NULL;
-	}
-	
-	pthread_detach(pthread_self());
-	while(!csa->done){
-		int index;
-		QNode *order;
-		
-		pthread_mutex_lock(&consumerstruct[index].mutex);
-		while (consumerstruct[index].q->numElem == 0)
-		{
-			pthread_cond_signal(&consumerstruct[index].spaceAvailable); // shout at producer
-			printf("Consumer thread '%s' waits for producer because of empty queue.\n", consumerstruct[index].category);
-			pthread_cond_wait(&consumerstruct[index].dataAvailable, &consumerstruct[index].mutex);
+		else {
+			printf("=== EMPTY LIST ===\n");
 		}
-		printf("Consumer thread '%s' resuming order processing.\n", consumerstruct[index].category);
-		order = pop(consumerstruct->q);
-		index = binarySearch(cdb, order->id, 0, cdb->numCust);
-		if (index >= 0) {
-			pthread_mutex_lock(&cdb->dbarray[index].queue_mutex);
-			CDUpdate(cdb, order);
-		}
-		free(order->bname);
-		free(order->category);
-		free(order);
-		pthread_cond_signal(&consumerstruct[index].spaceAvailable); // shout at producer
-		pthread_mutex_unlock(&consumerstruct[index].mutex);
+		printf("=== END CUSTOMER INFO ===\n");
+		printf("\n\n");
 	}
-	return NULL;
-}
-/*------------HELPER FUNCTIONS-------------*/
-
-void printCustomer(Customer *dude){
-	printf("---PRINTING CUSTOMER---\n");
-	printf("Customer Name: %s\n", dude->name);
-	printf("Customer ID: %ld\n", dude->id);
-	printf("Customer Balance: %f\n", dude->balance);
-	printf("Customer Address: %s\n", dude->address);
-	printf("Customer State: %s\n", dude->state);
-	printf("Customer Zip: %s\n", dude->zip);
-	printf("---END PRINTING CUSTOMER---\n");
 }
